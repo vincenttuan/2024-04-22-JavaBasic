68cdebf2bca83e0c01564740333ca79c17312801
VincentTuan <vincentjava@yahoo.com.tw>
2024-04-29 21:31:13 +0800

    Add files via upload

--- javacode/day3/Demo5.java
+++ javacode/day3/Demo5.java
@@ -23,7 +23,20 @@ public class Demo5 {
 		// 顯示最新停車資訊
 		System.out.printf("parking: %d%n", parking);
 		System.out.printf("parking: %s%n", Integer.toBinaryString(parking));
-
+		//-------------------------------------------------------------------
+		System.out.print("要移車-請輸入車位:");
+		n = sc.nextInt();
+		// 判斷 n 車位是否可以移車
+		if((parking & (int)Math.pow(2, n)) == 0) {
+			System.out.printf("%d 車位目前無車可移%n", n);
+			return; // 離開方法
+		}
+		System.out.printf("%d 車位可移車%n", n);
+		// 將車子開出
+		parking -= (int)Math.pow(2, n);
+		// 顯示最新停車資訊
+		System.out.printf("parking: %d%n", parking);
+		System.out.printf("parking: %s%n", Integer.toBinaryString(parking));
 	}
 
 }
