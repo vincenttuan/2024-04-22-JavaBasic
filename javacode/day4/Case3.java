c5e38b4aec13ea3d47b6709a2b53658e7a42fc4a
VincentTuan <vincentjava@yahoo.com.tw>
2024-05-03 19:32:53 +0800

    Add files via upload

---
+++ javacode/day4/Case3.java
@@ -0,0 +1,18 @@
+package day4;
+
+import java.util.Scanner;
+
+public class Case3 {
+
+	public static void main(String[] args) {
+		// 小明本次國英數考試成績: 85, 54, 93
+		// 請列出國英數分數並判斷及格或不及格
+		// 例如: 國:85(及格) 英:54(不及格) 數:93(及格) 總分:? 平均:?
+		// ps: 平均要到小數點1位
+		Scanner scanner = new Scanner(85, 54, 93);
+		
+
+
+	}
+
+}
