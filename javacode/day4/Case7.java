27998a44c356050f5e091d635515d9eaf191115f
VincentTuan <vincentjava@yahoo.com.tw>
2024-05-03 21:33:57 +0800

    Add files via upload

---
+++ javacode/day4/Case7.java
@@ -0,0 +1,20 @@
+package day4;
+
+/**
+使用者輸入分數 score (利用 Scanner)
+判斷級別(利用 switch-case)
+100 => A
+90 ~ 99 => A
+80 ~ 89 => B
+70 ~ 79 => C
+60 ~ 69 => D
+0 ~ 59 => F
+分數正常範圍 0~100
+若分數範圍不正確, 要印出"分數範圍不正確"
+*/
+public class Case7 {
+	public static void main(String[] args) {
+		
+
+	}
+}
