a28c6fa3f0e33a661e1c3a2ab7a97d6d8742e71c
VincentTuan <vincentjava@yahoo.com.tw>
2024-05-06 21:34:38 +0800

    Add files via upload

---
+++ javacode/day5/ForLoopDemo2.java
@@ -0,0 +1,18 @@
+package day5;
+
+public class ForLoopDemo2 {
+
+	public static void main(String[] args) {
+		
+		for(int x=1;x<=9;x++) {
+		
+			for(int y=1;y<=9;y++) {
+				System.out.printf("%d*%d=%d ", x, y, (x*y));
+			}
+			System.out.println();
+
+		}
+
+	}
+
+}
